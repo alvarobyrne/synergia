39587b7ff209a5f0baf3d5d0466e68e051dcc0ba
Alvaro Obyrne <alvarobyrne@gmail.com>
2020-10-16 23:18:26 -0500

    reading from a file

---
+++ readCSV.js
@@ -0,0 +1,32 @@
+const perlin = require('perlin');
+const dgram = require('dgram');
+const udp = dgram.createSocket("udp4");
+const osc = require('osc-min');
+// console.log('perlin: ', perlin);
+const fs = require('fs');
+const dataString = fs.readFileSync("data.csv",'utf8');
+// console.log('dataString: ', dataString);
+const dataSplitted = dataString.split('\n');
+// console.log('dataSplitted: ', dataSplitted);
+var data = dataSplitted.map(row => row.split(',').map(f=>+f));
+console.log('data: ', data);
+var a=0;
+const outport = 41234;
+sendHeartbeat = function() {
+    const row = data[a];
+    // console.log('row: ', row);
+    a+=1;
+    if(a>data.length-1){
+        a=0
+    }
+    var buf;
+    const args = row.map(x=>{return{type:"float",value:x}});
+    console.log('args: ', args);
+    buf = osc.toBuffer({
+      address: "/heartbeat",
+      args
+    });
+    return udp.send(buf, 0, buf.length, outport, "localhost");
+  };
+   
+  setInterval(sendHeartbeat, 100);
